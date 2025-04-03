6d07fc52b672b9ed11163383f1dd181a5e77a80c
Andtit4 <andtit4@gmail.com>
2025-04-03 21:48:44 +0000

    modif

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,6 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '500M',
       env: {
         NODE_ENV: 'production',
         PM2_SERVE_PATH: 'dist',
